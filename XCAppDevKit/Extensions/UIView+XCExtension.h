fee62908558b1f81bf83438076dab1b9287d6bc1
xingcheng <i@xingcheng.me>
2017-07-19 12:16:19 +0800

    v0.1.3

--- XCAppDevKit/Extensions/UIView+XCExtension.h
+++ XCAppDevKit/Extensions/UIView+XCExtension.h
@@ -9,6 +9,30 @@
 #import <UIKit/UIKit.h>
 
 @interface UIView (XCExtension)
+#pragma mark - hierarchy
+
+- (UIView *)viewMatchingPredicate:(NSPredicate *)predicate;
+- (UIView *)viewWithTag:(NSInteger)tag ofClass:(Class)viewClass;
+- (UIView *)viewOfClass:(Class)viewClass;
+- (NSArray *)viewsMatchingPredicate:(NSPredicate *)predicate;
+- (NSArray *)viewsWithTag:(NSInteger)tag;
+- (NSArray *)viewsWithTag:(NSInteger)tag ofClass:(Class)viewClass;
+- (NSArray *)viewsOfClass:(Class)viewClass;
+
+- (UIView *)firstSuperviewMatchingPredicate:(NSPredicate *)predicate;
+- (UIView *)firstSuperviewOfClass:(Class)viewClass;
+- (UIView *)firstSuperviewWithTag:(NSInteger)tag;
+- (UIView *)firstSuperviewWithTag:(NSInteger)tag ofClass:(Class)viewClass;
+
+- (BOOL)viewOrAnySuperviewMatchesPredicate:(NSPredicate *)predicate;
+- (BOOL)viewOrAnySuperviewIsKindOfClass:(Class)viewClass;
+- (BOOL)isSuperviewOfView:(UIView *)view;
+- (BOOL)isSubviewOfView:(UIView *)view;
+
+- (UIViewController *)firstViewController;
+- (UIView *)firstResponder;
+
+#pragma mark - frame
 @property (nonatomic, assign) CGSize  size;
 @property (nonatomic, assign) CGFloat x;
 @property (nonatomic, assign) CGFloat y;
--- XCAppDevKit/Extensions/UIView+XCExtension.m
+++ XCAppDevKit/Extensions/UIView+XCExtension.m
@@ -9,6 +9,159 @@
 #import "UIView+XCExtension.h"
 
 @implementation UIView (XCExtension)
+#pragma mark - hierarchy
+- (UIView *)viewMatchingPredicate:(NSPredicate *)predicate
+{
+    if ([predicate evaluateWithObject:self])
+    {
+        return self;
+    }
+    for (UIView *view in self.subviews)
+    {
+        UIView *match = [view viewMatchingPredicate:predicate];
+        if (match) return match;
+    }
+    return nil;
+}
+
+- (UIView *)viewWithTag:(NSInteger)tag ofClass:(Class)viewClass
+{
+    return [self viewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, __unused NSDictionary *bindings) {
+        return [evaluatedObject tag] == tag && [evaluatedObject isKindOfClass:viewClass];
+    }]];
+}
+
+- (UIView *)viewOfClass:(Class)viewClass
+{
+    return [self viewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, __unused NSDictionary *bindings) {
+        return [evaluatedObject isKindOfClass:viewClass];
+    }]];
+}
+
+- (NSArray *)viewsMatchingPredicate:(NSPredicate *)predicate
+{
+    NSMutableArray *matches = [NSMutableArray array];
+    if ([predicate evaluateWithObject:self])
+    {
+        [matches addObject:self];
+    }
+    for (UIView *view in self.subviews)
+    {
+        //check for subviews
+        //avoid creating unnecessary array
+        if ([view.subviews count])
+        {
+            [matches addObjectsFromArray:[view viewsMatchingPredicate:predicate]];
+        }
+        else if ([predicate evaluateWithObject:view])
+        {
+            [matches addObject:view];
+        }
+    }
+    return matches;
+}
+
+- (NSArray *)viewsWithTag:(NSInteger)tag
+{
+    return [self viewsMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, __unused id bindings) {
+        return [evaluatedObject tag] == tag;
+    }]];
+}
+
+- (NSArray *)viewsWithTag:(NSInteger)tag ofClass:(Class)viewClass
+{
+    return [self viewsMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, __unused id bindings) {
+        return [evaluatedObject tag] == tag && [evaluatedObject isKindOfClass:viewClass];
+    }]];
+}
+
+- (NSArray *)viewsOfClass:(Class)viewClass
+{
+    return [self viewsMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, __unused id bindings) {
+        return [evaluatedObject isKindOfClass:viewClass];
+    }]];
+}
+
+- (UIView *)firstSuperviewMatchingPredicate:(NSPredicate *)predicate
+{
+    if ([predicate evaluateWithObject:self])
+    {
+        return self;
+    }
+    return [self.superview firstSuperviewMatchingPredicate:predicate];
+}
+
+- (UIView *)firstSuperviewOfClass:(Class)viewClass
+{
+    return [self firstSuperviewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIView *superview, __unused id bindings) {
+        return [superview isKindOfClass:viewClass];
+    }]];
+}
+
+- (UIView *)firstSuperviewWithTag:(NSInteger)tag
+{
+    return [self firstSuperviewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIView *superview, __unused id bindings) {
+        return superview.tag == tag;
+    }]];
+}
+
+- (UIView *)firstSuperviewWithTag:(NSInteger)tag ofClass:(Class)viewClass
+{
+    return [self firstSuperviewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIView *superview, __unused id bindings) {
+        return superview.tag == tag && [superview isKindOfClass:viewClass];
+    }]];
+}
+
+- (BOOL)viewOrAnySuperviewMatchesPredicate:(NSPredicate *)predicate
+{
+    if ([predicate evaluateWithObject:self])
+    {
+        return YES;
+    }
+    return [self.superview viewOrAnySuperviewMatchesPredicate:predicate];
+}
+
+- (BOOL)viewOrAnySuperviewIsKindOfClass:(Class)viewClass
+{
+    return [self viewOrAnySuperviewMatchesPredicate:[NSPredicate predicateWithBlock:^BOOL(UIView *superview, __unused id bindings) {
+        return [superview isKindOfClass:viewClass];
+    }]];
+}
+
+- (BOOL)isSuperviewOfView:(UIView *)view
+{
+    return [self firstSuperviewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(UIView *superview, __unused id bindings) {
+        return superview == view;
+    }]] != nil;
+}
+
+- (BOOL)isSubviewOfView:(UIView *)view
+{
+    return [view isSuperviewOfView:self];
+}
+
+//responder chain
+
+- (UIViewController *)firstViewController
+{
+    id responder = self;
+    while ((responder = [responder nextResponder]))
+    {
+        if ([responder isKindOfClass:[UIViewController class]])
+        {
+            return responder;
+        }
+    }
+    return nil;
+}
+
+- (UIView *)firstResponder
+{
+    return [self viewMatchingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, __unused id bindings) {
+        return [evaluatedObject isFirstResponder];
+    }]];
+}
+#pragma mark - frame
 - (void)setSize:(CGSize)size
 {
     CGRect frame = self.frame;
@@ -65,7 +218,6 @@
 
 - (void)setRoundedCorners:(UIRectCorner)corners radius:(CGSize)size {
     UIBezierPath* maskPath = [UIBezierPath bezierPathWithRoundedRect:self.bounds byRoundingCorners:corners cornerRadii:size];
-    
     CAShapeLayer* maskLayer = [CAShapeLayer new];
     maskLayer.frame = self.bounds;
     maskLayer.path = maskPath.CGPath;
