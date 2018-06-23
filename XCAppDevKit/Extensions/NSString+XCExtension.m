470a114e575876faf2b32db0436c0159e3e240c2
Ruby Xing <xingchengboy@126.com>
2018-06-23 13:34:23 +0800

    fix bug

--- XCAppDevKit/Extensions/NSString+XCExtension.m
+++ XCAppDevKit/Extensions/NSString+XCExtension.m
@@ -426,12 +426,12 @@
     }
     return string;
 }
-- (NSString *)removeEmoji:(NSString *)text
+- (NSString *)removeEmoji
 {
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"[^\\u0020-\\u007E\\u00A0-\\u00BE\\u2E80-\\uA4CF\\uF900-\\uFAFF\\uFE30-\\uFE4F\\uFF00-\\uFFEF\\u0080-\\u009F\\u2000-\\u201f\r\n]" options:NSRegularExpressionCaseInsensitive error:nil];
-    NSString *modifiedString = [regex stringByReplacingMatchesInString:text
+    NSString *modifiedString = [regex stringByReplacingMatchesInString:self
                                                                options:0
-                                                                 range:NSMakeRange(0, [text length])
+                                                                 range:NSMakeRange(0, [self length])
                                                           withTemplate:@""];
     return modifiedString;
 }
